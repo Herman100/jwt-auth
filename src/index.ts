e85afd127ccf1293fdff2232d44d3c2f4c5a150c
Herman100 <hermankwamebour30@gmail.com>
2025-05-05 21:50:56 +0000

    feat: add nodemon configuration and update package.json scripts for development

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendStatus(200).send("Welcome to the JWT Authentication API!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
